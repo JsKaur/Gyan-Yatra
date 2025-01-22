39d55da53265105a1eb7f54dfe5c17f037b581a7
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-22 08:57:54 -0500

    designing the main page

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Trivia() {
+  return (
+    <div className="trivia">
+      <div className="question">What is best you tube channel?</div>
+      <div className="answers">
+        <div className="answer">Code With Harry</div>
+        <div className="answer">Siddhu</div>
+        <div className="answer">Jatt beat</div>
+        <div className="answer">Lama Dev</div>
+      </div>
+    </div>
+  )
+}
