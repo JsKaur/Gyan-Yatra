c7480642ce20c0c5ea8ccb3dec4f83bf3d7bfe13
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-23 11:31:34 -0500

    added animations to answers

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Trivia() {
   return (
     <div className="trivia">
-      <div className="question">What is best you tube channel?</div>
+      <div className="question">What is your name?</div>
       <div className="answers">
-        <div className="answer">Code With Harry</div>
+        <div className="answer">Harry</div>
         <div className="answer">Siddhu</div>
-        <div className="answer">Jatt beat</div>
-        <div className="answer">Lama Dev</div>
+        <div className="answer">Jashan</div>
+        <div className="answer">Lama</div>
       </div>
     </div>
   )
