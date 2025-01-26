9e6e12394c3b96b5be204663a27f0536c6ba40ec
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-25 19:09:43 -0500

    added sounds

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
+import {useSound} from 'use-sound';
+import play from '../assets/play.mp3';
+import wrong from '../assets/wrong.mp3';
+import correct from '../assets/correct.mp3';
+//import wait from '../assets/wait.mp3';
 
 export default function Trivia({data, questionNumber, setQuestionNumber, setStop}) {
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName]=useState("answer");
+  const [isStarted, setIsStarted] = useState(false);
+
+  const [playSound]=useSound(play);
+  const [correctAnswer]=useSound(correct);
+  const [wrongAnswer]=useSound(wrong);
+  
+  //const [waitSound]=useSound(wait);
+
+  useEffect(()=>{
+    playSound();
+  },[playSound,questionNumber]);
+
+  const handleStart = () => {
+    setIsStarted(true);
+    playSound(); // Play sound after a user gesture
+  };
 
   const delay=(duration,callback)=>{
-    setTimeout(()=>{
-      callback();
-    
-  },duration);
+    setTimeout(callback,duration);
 };
 
 const handleClick = (a) => {
@@ -20,12 +38,21 @@ const handleClick = (a) => {
     setClassName(a.correct ? "answer correct" : "answer wrong")
   );
 
-  delay(6000, () => {
+  delay(5000, () => {
     if (a.correct) {
-      setQuestionNumber((prev) => prev + 1);
-      setSelectedAnswer(null);
+      correctAnswer();
+      delay(1000, () => {
+        setQuestionNumber((prev) => prev + 1);
+        setSelectedAnswer(null);
+      });
+      
     } else {
-      setStop(true);
+      wrongAnswer();
+      delay(1000, () => {
+        
+        setStop(true);
+      });
+      
     }
   });
 };
@@ -34,13 +61,22 @@ useEffect(() => {
   setQuestion(data[questionNumber - 1]);
 }, [questionNumber, data]);
 
+useEffect(() => {
+  if (isStarted) {
+    setQuestion(data[questionNumber - 1]);
+  }
+}, [isStarted, questionNumber, data]);
+
+if (!isStarted) {
+  return <button onClick={handleStart}>Start Trivia</button>;
+}
 
   return (
     <div className="trivia">
       <div className="question">{question?.question}</div>
       <div className="answers">
-        {question?.answers.map((a)=>(
-          <div className={selectedAnswer===a? className:"answer"} onClick={()=>handleClick(a)}>{a.text}</div>
+        {question?.answers.map((a,index)=>(
+          <div key={index} className={selectedAnswer===a? className:"answer"} onClick={()=>handleClick(a)}>{a.text}</div>
         ))}
         
       </div>
